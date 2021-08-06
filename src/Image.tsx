7c37dac545d57f8a126d592268a8978cd50c1065
francisco <francisco@filmin.es>
2021-08-06 09:35:28 +0200

    Initial commit

---
+++ src/Image.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { useEffect, useState, useMemo } from 'react';
+import { Box, Text } from 'ink';
+import AsciiArtImage from 'ascii-art-image';
+
+interface Props {
+	filePath: string;
+	width?: number;
+	height?: number;
+	alphabet?:
+		| 'variant1'
+		| 'variant2'
+		| 'variant3'
+		| 'variant4'
+		| 'ultra-wide'
+		| 'wide'
+		| 'hatching'
+		| 'bits'
+		| 'binary'
+		| 'greyscale'
+		| 'blocks';
+}
+
+export const Image: React.FC<Props> = ({
+	filePath,
+	width,
+	height,
+	alphabet = 'blocks',
+}) => {
+	const [imageText, setImageText] = useState();
+
+	useEffect(() => {
+		const image = new AsciiArtImage({
+			filepath: filePath,
+			alphabet,
+			width,
+			height,
+		});
+		image.write((err, rendered) => {
+			if (err) {
+				console.error(err);
+			}
+			setImageText(rendered);
+		});
+	}, [filePath, alphabet, width, height]);
+
+	return (
+		<Box with={width} height={height}>
+			<Text>{imageText}</Text>
+		</Box>
+	);
+};
